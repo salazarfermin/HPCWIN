7aadcd9a8f469649492ee3cf8d00f0ed50850f0d
salazarfermin <salazarfermin@hotmail.com>
2017-01-21 22:32:51 -0600

    Update 1

--- HPCWin.ps1
+++ HPCWin.ps1
@@ -2,15 +2,16 @@
 #[string]$SubscriptionId = '86e16eab-9149-438c-b23f-042a43311e1c';
 [string]$ResourceGroupName = 'DCPNTI-GSPI-WinHPC';
 #[string]$Location = 'South Central US';
-[string]$Location = 'southcentralus';
+#[string]$Location = 'southcentralus';
+[string]$Location = 'eastus';
 
-Login-AzureRmAccount -SubscriptionId $SubscriptionId;
+#Login-AzureRmAccount -SubscriptionId $SubscriptionId;
 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location;
 
 New-AzureRmResourceGroupDeployment -Name hpc-ctest-vnet -ResourceGroupName $ResourceGroupName -TemplateFile (Join-Path (Split-Path $myinvocation.mycommand.path) 'vnet\vnetdeploy.json') -TemplateParameterFile (Join-Path (Split-Path $myinvocation.mycommand.path) 'vnet\parameters.json') -Verbose;
 
-#New-AzureRmResourceGroupDeployment -Name hpc-ctest -ResourceGroupName $ResourceGroupName -TemplateFile (Join-Path (Split-Path $myinvocation.mycommand.path) 'HPCLinux.json') -Verbose -DeploymentDebugLogLevel All;
+New-AzureRmResourceGroupDeployment -Name hpc-ctest -ResourceGroupName $ResourceGroupName -TemplateFile (Join-Path (Split-Path $myinvocation.mycommand.path) 'azuredeploy.json') -Verbose -DeploymentDebugLogLevel All;
 
 #Remove-AzureRmResourceGroup -Name $ResourceGroupName;
 
